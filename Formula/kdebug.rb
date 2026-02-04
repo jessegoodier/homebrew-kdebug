6461274d38a750156f8366563a031f7f11fee1a8
jesse goodier <31039225+jessegoodier@users.noreply.github.com>
2026-02-03 22:10:22 -0500

    v0.1.0

--- Formula/kdebug.rb
+++ Formula/kdebug.rb
@@ -2,7 +2,7 @@ class Kdebug < Formula
   desc "Universal Kubernetes Debug Container Utility"
   homepage "https://github.com/jessegoodier/homebrew-kdebug"
   url "https://github.com/jessegoodier/kdebug/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 "3d439253304d37ca32b0e6cf42a11c5f796dedf33b77e49c8abcc91e9ac21c60"
   license "MIT"
   version "0.1.0"
 
